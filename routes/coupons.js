91182eb1bc40ebc1cc5623359da415098603a596
John Wu <webmaster@leapoahead.com>
2015-04-02 16:04:24 +0800

    Coupon search API

--- routes/coupons.js
+++ routes/coupons.js
@@ -3,6 +3,28 @@ var router = express.Router();
 
 var Coupon = require('../lib/Schema/Coupon');
 
+/* GET search coupons listing */
+router.get('/search', function(req, res) {
+  var query = {
+    keyword: req.query['keyword'],
+    field: req.query['field'] || 'title'
+  };
+  if (!query.keyword) {
+    return res.json({error: 'Keyword is required'});
+  }
+
+  var filterObj = {};
+  filterObj[query.field] = new RegExp(query.keyword, 'i');
+
+  Coupon
+    .find(filterObj)
+    .limit(query.records)
+    .skip(query.records * (query.page - 1))
+    .exec().then(function(coupons) {
+      res.json(coupons);
+    });
+});
+
 /* GET coupons listing. */
 router.get('/', function(req, res) {
   var query = {
