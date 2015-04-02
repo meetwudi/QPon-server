bb3b0ed07c29f289934b5873d5c5ede507d9e86d
John Wu <webmaster@leapoahead.com>
2015-04-02 10:32:54 +0800

    YP.com fetcher

--- test/lib/fetcher/yp-spec.js
+++ test/lib/fetcher/yp-spec.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var YP = require('../../../lib/fetcher/YP');
+
+describe('YP fetcher', function() {
+  it('should fetch from server and store to data store', function(done) {
+    YP.exec().then(function(coupons) {
+      done();
+    });
+  });
+});
