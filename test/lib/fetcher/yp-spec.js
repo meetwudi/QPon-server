e83b8a346988408fa2df8e8b5ec95d78b7563c25
John Wu <webmaster@leapoahead.com>
2015-04-02 15:29:18 +0800

    Error tolerance

--- test/lib/fetcher/yp-spec.js
+++ test/lib/fetcher/yp-spec.js
@@ -4,7 +4,7 @@ var YP = require('../../../lib/fetcher/YP');
 
 describe('YP fetcher', function() {
   it('should fetch from server and store to data store', function(done) {
-    YP.exec().then(function(coupons) {
+    YP.exec().then(function() {
       done();
     });
   });
