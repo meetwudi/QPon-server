5ab4df3dd71e805b5f9d7832684d0a243109b48a
John Wu <webmaster@leapoahead.com>
2015-04-02 11:15:16 +0800

    Remove db open test

--- test/lib/db/db-spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var db = require('../../../lib/db/db');
-
-describe('MongoDB', function() {
-  it('should be opened', function(done) {
-    db.connection.once('open', done);
-  });
-});
